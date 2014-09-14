bbef0166747ef07c2a1daad4f35621cc7363bd50
renyedi <renyedi@ebay.com>
2014-09-13 23:13:26 -0700

    Integrated swipe refresh

--- SKHInstagramViewer/src/com/codepath/skyhalud/skhinstagramviewer/PhotosActivity.java
+++ SKHInstagramViewer/src/com/codepath/skyhalud/skhinstagramviewer/PhotosActivity.java
@@ -10,6 +10,8 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -26,15 +28,41 @@ public class PhotosActivity extends Activity {
 	
 	private ArrayList<InstagramPhoto> photos;
 	private InstagramPhotosAdapter aPhotos;
+
+	private SwipeRefreshLayout swipeContainer;
+
+	private AsyncHttpClient ahc;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photos);
         
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+            	fetchPopularPhotos();
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                fetchTimelineAsync(0);
+            } 
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorScheme(android.R.color.holo_blue_bright, 
+                android.R.color.holo_green_light, 
+                android.R.color.holo_orange_light, 
+                android.R.color.holo_red_light);
+        
         fetchPopularPhotos();
     }
 
+    public void fetchTimelineAsync(int page) {
+    	 swipeContainer.setRefreshing(false);
+    }
+    
 
     private void fetchPopularPhotos() {
     	// Hit: https://api.instagram.com/v1/media/popular?client_id=8a2f29f159d84f06af4dfedfb6fc9a7d
@@ -48,7 +76,7 @@ public class PhotosActivity extends Activity {
     	
     	Toast.makeText(PhotosActivity.this.getBaseContext(), "Loading photos...", Toast.LENGTH_LONG).show();
     	
-    	AsyncHttpClient ahc = new AsyncHttpClient();
+    	ahc = new AsyncHttpClient();
     	ahc.setTimeout((int) TimeUnit.SECONDS.toMillis(5));
     	ahc.get(popularUrl, new JsonHttpResponseHandler() {
 
